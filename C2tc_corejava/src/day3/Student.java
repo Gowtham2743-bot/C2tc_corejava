68ca1eaa3932d6a4c2d44cc3656f82be25ab0c25
DELL <DELL@TVK>
2025-06-20 09:26:30 +0530

    JAVA

---
+++ C2tc_corejava/src/day3/Student.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class Student {
+	
+	public static void main (String[] args) {
+		
+		
+	}
+
+}
---
+++ C2tc_corejava/src/day3/StudentDemo.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class StudentDemo {
+	
+	public static void main (String[] args) {
+		
+		Student s= new Student();
+		System.out.println(s);
+		
+		Student s1= new Student();
+		
+		
+	}
+
+}
